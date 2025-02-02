aaea8d6ddafa394850ecda0e4909ad418f920bca
Martin <mcsato@seznam.cz>
2025-02-02 18:05:13 +0100

    nefunguje save trida 5evod na json

--- API/APIrequests.py
+++ API/APIrequests.py
@@ -2,12 +2,19 @@ import requests
 import json
 import os
 import Tridy
+import random
+import string
+
 # from Tridy import Elektro
+
+def randomSlovo(delka=10):
+    return ''.join(random.choices(string.ascii_letters, k=delka))
 
 def apiAll(url):
     print("začala metoda Všechny")
     # verify=False (rychlé, ale nebezpečné)
-    response = requests.get(url, verify=False)
+    headers = {"Content-Type": "application/json"}  
+    response = requests.get(url, verify=False, headers=headers)
     if not response.status_code == 200:
         print("chyba")
         exit()
@@ -32,10 +39,13 @@ def apiPost(url, data):
         print("✅ Úspěšně odesláno!")
     else:
         print(f"⚠️ Chyba při odesílání: {response.status_code}")
-
-    # response = requests.post(url, json=data)
+    
     Out = response.json()
-    return Out
+    print("response jako slovnik " ,Out)
+
+    trida = Tridy.Elektro.json(Out)
+
+    return trida
 
 def apiIDelete(url, id):
     print("začala metoda delete")
@@ -44,31 +54,31 @@ def apiIDelete(url, id):
     # response = requests.delete(url, headers=headers)
 
     # Nahrazení {id} za skutečné ID
-    # urlLink = 'https://example.com/resource/{id}'
-    # response = requests.delete(urlLink.format(id=123))
     urlLink = f"{url}/{id}"
     print(urlLink)
-    response = requests.delete(urlLink)
+    headers = {"Content-Type": "application/json"}  
+    response = requests.delete(urlLink, headers=headers)
     # response = requests.delete(urlLink.format(id = id))
     if response.status_code != 200:
         print("Chyba ", response)
         exit()
     print("záznam byl smazán ", response)
-    
-    # Out = response.json()
-    # print("response jako slovnik " ,Out)
-    
-    # text  = response.text
-    # print("Toto je text ", text)
-
-    # trida = Tridy.Elektro.json(Out)
     return 
 
 def apiPut(url, data):
-    response = requests.post(url, json=data)
+    headers = {"Content-Type": "application/json"}  
+    response = requests.post(url, json=data, headers=headers)
     Out = response.json()
     return Out
 
+# Export dat pro profese
+class ElekroEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if hasattr(obj, "__dict__"):
+            return obj.__dict__
+        return super().default(obj)
+
+# Jedna se o testovaní uvedených metod
 if __name__ == "__main__":
     os.system("cls")
     # Funguje server musí být spuštěn
@@ -76,41 +86,42 @@ if __name__ == "__main__":
     apiUrl = "http://localhost/api/elektro"
     print(apiUrl)
     
-    # Všechny záznamy
-    rest = apiAll(apiUrl)
-    print(rest)
+    # Všechny záznamy vratí seznam tříd Elektro
+    restultAll = apiAll(apiUrl)
+
 
     # Přidat záznam
-    name = "xxxx" 
-    popis = "dddd"
-    data = {"name": name, "popis": popis}
+    data = {"name": randomSlovo(5), "popis": randomSlovo(10)}
     Pole = apiPost(apiUrl, data)
     print(Pole)
 
 
-    if rest:
-        lastData = rest[-1]
+    if restultAll:
+        lastData = restultAll[-1]
         print("Poslední zaznam: " , lastData.apid)
-        # print("Poslední aspid: " , lastData['apid'])
-
-        # pokus o převod na trřídu
-        # lastRecord = Tridy.Elektro.json(lastData)
-        # lastRecord = lastData
-        # Apid = lastRecord['apid']       
-        # Apid = lastRecord.apid
-        # print("Apid: " , Apid)
-    
-        # Delete dle apid
-        # headers = {'apid': lastRecord.apid}
-        # print("Apid: " , headers)
         data = apiIDelete(apiUrl, lastData.apid)  
         print("Toto je smazanáý záznam", data)
-        # print(list)  
 
     # Save to a file
+    print()
+    print("Save sample.json ")
+    
+    data = json.dumps(restultAll, cls=ElekroEncoder, indent=4)
+    print(data)
+
     with open("sample.json", "w") as outfile:
+        outfile.write(data)
         # indent je počet mezer ve formátu json
-        json.dump(rest, outfile, indent=4)
+        # json.dump([vars(obj) for obj in rest],  outfile=outfile, indent=4, ensure_ascii=False, cls=DateEncoder)
+        # json_data = json.dumps([vars(obj) for obj in restultAll], indent=4)
+
+        # for obj in restultAll:
+            # print(vars(obj))
+        
+
+        # json.dump([vars(obj) for obj in restultAll],  outfile=outfile, indent=4)
+        # json.dumps([vars(obj) for obj in restultAll], outfile, indent=4)
+        # json.dumps(restultAll, outfile, indent=4)
 
     with open("sample.json", "r") as outfile:
         data = json.load(outfile)
