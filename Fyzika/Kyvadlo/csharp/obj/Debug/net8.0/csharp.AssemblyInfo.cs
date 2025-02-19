59891ab61bdb1edc8c7e8611d78cc88ae2bdc720
Martin <56979879+Martin74Cs@users.noreply.github.com>
2025-02-19 10:40:23 +0100

    Dedičnost

--- Fyzika/Kyvadlo/csharp/obj/Debug/net8.0/csharp.AssemblyInfo.cs
+++ Fyzika/Kyvadlo/csharp/obj/Debug/net8.0/csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e68b44bf55418ff6f3f7f5366894d79ba02a7b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c11d280a3254c6b00b9358df2ae9a8e29db312b")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Testy/Class/Dedicnost.py
@@ -0,0 +1,110 @@
+# from abc import ABC, abstractmethod
+
+# class IEntity:
+#     """Rozhraní pro všechny entity."""
+#     @abstractmethod
+#     def get(self, id):
+#         pass
+
+#     @abstractmethod
+#     def set(self, id, value):
+#         pass
+
+class Entity:
+
+    def __init__(self, id=None, name=None):
+        self.Id  = id
+        self.Name = name
+        # super().__init__(self,id=None)  # Initialize Entity attributes
+
+    # @property
+    # def Id(self):
+    #     return self.Id
+
+    # @Id.setter
+    # def Id(self, x):
+    #     self.Id = x
+
+    # @property
+    # def Name(self):
+    #     return self.Name
+
+    # @Name.setter
+    # def Name(self, x):
+    #     self.Name = x
+
+
+class Elektro(Entity):
+
+    def __init__(self, id=None, name=None, hmotnost=None):
+        super().__init__(id, name)  # Initialize Entity attributes
+        self.Hmotnost = hmotnost
+
+    # @property
+    # def Hmotnost(self):
+    #     return self.Hmotnost
+
+    # @Hmotnost.setter
+    # def Hmotnost(self, x):
+    #     self.Hmotnost = x
+
+    def __str__(self):
+        return f"Elektro(ID={self.get('id')}, Name={self.get('name')}, Hmotnost={self.get('hmotnost')})"
+    
+class Stroj(Entity):
+
+    def __init__(self, id=None, name=None, velikost=None):
+        super().__init__(id, name)  # Initialize Entity attributes
+        self.Velikost = velikost
+
+    @property
+    def Velikost(self):
+        return self.Velikost
+
+    @Velikost.setter
+    def Velikost(self, x):
+        self.Velikost = x
+
+def Select(entity: enumerate[Entity], find: int) -> Entity | None:
+    pole = []
+    for znak in entity:
+        if(znak.Id == find):
+            pole.append(znak)
+            return znak
+    # return pole
+
+def SelectPodmínka(entity: enumerate[Entity], Podmínka) -> enumerate[Entity] | None:
+    pole = []
+    for znak in entity:
+        if(Podmínka):
+            pole.append(znak)
+    return pole
+
+Pole = []
+Test = Elektro()
+Test.Id = 10
+Test.Name = "dafsd"
+Test.Hmotnost = 1234
+Pole.append(Test)
+
+Pole.append(Elektro(1,"Jedna",100))
+Pole.append(Elektro(2,"Dva",200))
+Pole.append(Elektro(3,"Tři",300))
+Vyber = Select(Pole, 3)
+print(Vyber.Hmotnost)
+Vyber = SelectPodmínka(Pole, lambda e: e.get("Id") > 1)
+print("Počet :", len(Vyber))
+for znak in Vyber:
+    print(znak)
+
+Pole = []
+Test = Stroj()
+Test.Id = 10
+Test.Name = "dafsd"
+Test.Velikost = 1234
+Pole.append(Stroj(1,"Jedna",100))
+Pole.append(Stroj(2,"Dva",200))
+Vyber = Select(Pole, 2)
+print(Vyber.Velikost)
+
+
